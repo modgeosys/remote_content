bcd55a324ff35b43cb2cdc7cb84f3fdc04fd7de8
Kevin Weller <kweller@asapwebsoft.com>
2010-09-30 14:44:50 -0600

    Initial commit.

---
+++ lib/tasks/remote_content.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :remote_content do
+#   # Task goes here
+# end
---
+++ test/remote_content_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class RemoteContentTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
